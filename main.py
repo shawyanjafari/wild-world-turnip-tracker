d7cb1a839422d4291146ac75ed3175f37238e829
shawyanjafari <shawyanjafari@users.noreply.github.com>
2022-02-27 13:09:38 -0600

    revise logic for separate turnip-buying-prices csv file

--- main.py
+++ main.py
@@ -6,16 +6,24 @@ today = dt.date.today().weekday()
 csv_headers = 'date,morning_price,afternoon_price'
 input_accepted = False
 
-# if file exists, record turnip prices for the day
-with open('turnip_prices.csv', 'a+') as f:
-    if today == 6:  # turnips can't be sold on Sundays
-        f.write(f'{datetime.date.today()},{np.nan},{np.nan}\n')
-    else:
-        while not input_accepted:
-            try:
-                morning_turnip_prices = int(input('Please enter morning turnip prices: '))
-                afternoon_turnip_prices = int(input('Please enter afternoon turnip prices: '))
-                input_accepted = True
-                f.write(f'{datetime.date.today()},{morning_turnip_prices},{afternoon_turnip_prices}\n')
-            except ValueError:
-                print("You must enter a number!")
+# check current day
+# if sunday, record buying price, else record morning/afternoon selling price
+if today == 6:
+    with open('turnip_buying_prices.csv', 'a+') as f:
+        print('Turnips can only be *purchased* on Sundays.')
+        buying_price = int(input('Please enter buying price of turnips: '))
+        f.write(f'{datetime.date.today()},{buying_price}\n')
+else:
+    with open('turnip_prices.csv', 'a+') as f:
+        if today == 6:  # turnips can't be sold on Sundays
+            print('Turnips can only be *purchased* on Sundays.')
+            f.write(f'{datetime.date.today()},{np.nan},{np.nan}\n')
+        else:
+            while not input_accepted:
+                try:
+                    morning_turnip_prices = int(input('Please enter morning turnip prices: '))
+                    afternoon_turnip_prices = int(input('Please enter afternoon turnip prices: '))
+                    input_accepted = True
+                    f.write(f'{datetime.date.today()},{morning_turnip_prices},{afternoon_turnip_prices}\n')
+                except ValueError:
+                    print("You must enter a number!")
