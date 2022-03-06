b7202b748127aa84e1c29cfa3ece15f77f51d21b
shawyanjafari <shawyanjafari@users.noreply.github.com>
2022-03-06 09:53:19 -0600

    update csv files and main script

--- main.py
+++ main.py
@@ -13,17 +13,15 @@ if today == 6:
         print('Turnips can only be *purchased* on Sundays.')
         buying_price = int(input('Please enter buying price of turnips: '))
         f.write(f'{datetime.date.today()},{buying_price}\n')
-else:
-    with open('turnip_selling_prices.csv', 'a+') as f:
-        if today == 6:  # turnips can't be sold on Sundays
-            print('Turnips can only be *purchased* on Sundays.')
-            f.write(f'{datetime.date.today()},{np.nan},{np.nan}\n')
-        else:
-            while not input_accepted:
-                try:
-                    morning_turnip_prices = int(input('Please enter morning turnip prices: '))
-                    afternoon_turnip_prices = int(input('Please enter afternoon turnip prices: '))
-                    input_accepted = True
-                    f.write(f'{datetime.date.today()},{morning_turnip_prices},{afternoon_turnip_prices}\n')
-                except ValueError:
-                    print("You must enter a number!")
+with open('turnip_selling_prices.csv', 'a+') as f:
+    if today == 6:  # turnips can't be sold on Sundays
+        f.write(f'{datetime.date.today()},{np.nan},{np.nan}\n')
+    else:
+        while not input_accepted:
+            try:
+                morning_turnip_prices = int(input('Please enter morning turnip prices: '))
+                afternoon_turnip_prices = int(input('Please enter afternoon turnip prices: '))
+                input_accepted = True
+                f.write(f'{datetime.date.today()},{morning_turnip_prices},{afternoon_turnip_prices}\n')
+            except ValueError:
+                print("You must enter a number!")
